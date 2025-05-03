69bb18ec3cbc02f28e6b1519b21453b3dfd6ef21
Aglo <113240095+Muminwilmer@users.noreply.github.com>
2025-05-04 00:46:44 +0200

    Add files via upload

--- polisen.py
+++ polisen.py
@@ -3,6 +3,7 @@ from pwnagotchi.ui.view import BLACK
 import pwnagotchi.ui.fonts as fonts
 import pwnagotchi.plugins as plugins
 import pwnagotchi
+import locale
 import logging
 import requests
 import re
@@ -10,7 +11,7 @@ from datetime import datetime
 
 
 class Polisen(plugins.Plugin):
-    __author__ = '@Muminwilmer'
+    __author__ = '@Mumin'
     __version__ = '1.3.0'
     __license__ = 'GPL3'
     __description__ = 'Displays the latest event from the Swedish police offical API.'
@@ -19,31 +20,19 @@ class Polisen(plugins.Plugin):
         self.city = None
         self.event = None
         self.connection = False
-        # Amount of epochs between update attempts
-        if 'epoch-wait' not in self.options:
-            self.options['epoch-wait'] = 0
-        # Only try to update while on wifi
-        if 'onlyOnInternet' not in self.options:
-            self.options['onlyOnInternet'] = False 
-        # If true it will show the latest new event. If false it will show latest update/new event
         if 'newestEventTop' not in self.options:
             self.options['newestEventTop'] = True
-        if 'twoUi' not in self.options:
-            self.options['twoUi'] = True
-        self.epochsWaited = 0
         logging.info("[Polisen] loaded!")
 
     def on_internet_available(self, agent):
         self.connection = True
         try:
-            if self._ui and any(self._ui.get(ui_key) in ('No wifi', '') for ui_key in ['event-polisen-ui', 'single-polisen-ui']):
-                if self.options['twoUi']:
-                    self._ui.set('city-polisen-ui', "Got wifi!")
-                    self._ui.set('event-polisen-ui', "Fetching news..")
-                else:
-                    self._ui.set('single-polisen-ui', "Got wifi Fetching..")
+            if self._ui and any(self._ui.get(ui_key) in ('No wifi', '') for ui_key in ['event-polisen-ui']):
+                self._ui.set('city-polisen-ui', "Got wifi!")
+                self._ui.set('event-polisen-ui', "Fetching news..")
         except Exception as e:
             logging.error(f"[Polisen] Pre internet on fetch failed: {e}")
+
         try:
             logging.info(f"[Polisen] Got wifi, Fetching.")
             if self.connection:
@@ -74,22 +63,16 @@ class Polisen(plugins.Plugin):
             else:
                 city_position = (0, 85)
                 event_position = (0, 92)
-            if self.options['twoUi']:
                 # Add the UI element
-                ui.add_element('city-polisen-ui', LabeledValue(
-                        color=BLACK, label='', value='',
-                        position=city_position, label_spacing=0,
-                        label_font=fonts.Small, text_font=fonts.Small, max_length=20))
-                # Add the UI element
-                ui.add_element('event-polisen-ui', LabeledValue(
-                        color=BLACK, label='', value='',
-                        position=event_position, label_spacing=0,
-                        label_font=fonts.Small, text_font=fonts.Small, max_length=20))
-            else:
-                ui.add_element('single-polisen-ui', LabeledValue(
+            ui.add_element('city-polisen-ui', LabeledValue(
+                    color=BLACK, label='', value='',
+                    position=city_position, label_spacing=0,
+                    label_font=fonts.Small, text_font=fonts.Small))
+            # Add the UI element
+            ui.add_element('event-polisen-ui', LabeledValue(
                     color=BLACK, label='', value='',
                     position=event_position, label_spacing=0,
-                    label_font=fonts.Small, text_font=fonts.Small, max_length=20))
+                    label_font=fonts.Small, text_font=fonts.Small))
             logging.info("[Polisen] UI element created successfully.")
             
         except Exception as e:
@@ -105,11 +88,8 @@ class Polisen(plugins.Plugin):
     def on_unload(self, ui):
         try:
             with ui._lock:
-                if self.options['twoUi']:
-                    ui.remove_element('city-polisen-ui')
-                    ui.remove_element('event-polisen-ui')
-                else:
-                    ui.remove_element('single-polisen-ui')
+                ui.remove_element('city-polisen-ui')
+                ui.remove_element('event-polisen-ui')
                 logging.info("[Polisen] Removed element.")
 
         except Exception as e:
@@ -119,23 +99,15 @@ class Polisen(plugins.Plugin):
     def on_ui_update(self, ui):
         try:
             if self.city:
-                if self.options['twoUi']:
-                    ui.set('city-polisen-ui', self.city)
-                    ui.set('event-polisen-ui', self.event)
-                    self.city = ""
-                    self.event = ""
-                else:
-                    ui.set('single-polisen-ui', f"{self.city[slice(12)]} - {self.event}")
-                    self.city = ""
-                    self.event = ""
+                ui.set('city-polisen-ui', self.city[slice(16)])
+                ui.set('event-polisen-ui', self.event[slice(16)])
+                self.city = ""
+                self.event = ""
                 ui.set('face', "(^-^)")
                 logging.info("[Polisen] ui has been updated!")
                 
-            elif not self.connection and self.options['onlyOnInternet'] and ui.get('event-polisen-ui') == '':
-                if self.options['twoUi']:
-                    ui.set('event-polisen-ui', "No wifi")
-                else:
-                    ui.set('single-polisen-ui', "No wifi")
+            elif not self.connection and ui.get('event-polisen-ui') == '':
+                ui.set('event-polisen-ui', "No wifi")
                 
         except Exception as e:
             logging.error(f"[Polisen] Failed to update UI: {e}")
@@ -143,25 +115,13 @@ class Polisen(plugins.Plugin):
     # Fetch every epoch (depending on your settings)
     def on_epoch(self, agent, epoch, epoch_data):
         try:
-            logging.info(f"[Polisen] S:{self.options['epoch-wait']} E:{self.epochsWaited}")
-            if (self.options['epoch-wait'] <= self.epochsWaited):
-                logging.info("[Polisen] New epoch!")
-                # Will check if it has internet before starting self.polisen()
-                # If check is turned off for some reason give a warning.
-                if self.options['onlyOnInternet']:
-                    if self.connection:
-                        self.polisen()
-                        self.epochsWaited = 0
-                    else:
-                        logging.warning("[Polisen] No connection - Skipping epoch")
-                else:
-                    if not self.connection:
-                        logging.warning("[Polisen] No connection - trying anyways as onlyOnInternet is False!")
-                    self.polisen()
-                    self.epochsWaited = 0
-            else: 
-                logging.info(f"[Polisen] Not yet - {self.options['epoch-wait'] - self.epochsWaited} epochs left before fetching news!")
-                self.epochsWaited += 1
+            logging.info("[Polisen] New epoch!")
+
+            if self.connection:
+                self.polisen()
+                self.epochsWaited = 0
+            else:
+                logging.warning("[Polisen] No connection - Skipping epoch")
         except Exception as e:
             logging.error(f"[Polisen] Start during epoch failed: {e}")
 
@@ -171,40 +131,66 @@ class Polisen(plugins.Plugin):
             logging.info(f"[Polisen] Fetching news!")
             response = requests.get("https://polisen.se/api/events", timeout=10)
             response.raise_for_status()
-    
+
             if response.status_code == 200:
                 data = response.json()
                 if data:
                     latest_event = data[0]
-    
-                    if self.options['newestEventTop']:
-                        latest_event = max(
-                            data,
-                            key=lambda event: datetime.strptime(
-                                re.search(r"(\S+) (\S+) (\d+\.\d+)", event.get('name', '')).group(0),
-                                f"%d %B %H.%M"
-                            )
-                        )
-    
+
+                    #if self.options['newestEventTop']:
+                    #    latest_event = max(
+                    #        data,
+                    #        key=lambda event: datetime.strptime(
+                    #            re.search(r"(\S+) (\S+) (\d+\.\d+)", event.get('name', '')).group(0),
+                    #            f"%d %B %H.%M"
+                    #        )		
+                    #    )
+                    if self.options['newestEventTop'] == True:
+                        month_translation = {
+                            "januari": "January",
+                            "februari": "February",
+                            "mars": "March",
+                            "april": "April",
+                            "maj": "May",
+                            "juni": "June",
+                            "juli": "July",
+                            "augusti": "August",
+                            "september": "September",
+                            "oktober": "October",
+                            "november": "November",
+                            "december": "December"
+                        }
+                        latest_time = None
+                        for event in data:
+                            name_str = event.get('name', '')
+
+                            # Extracting date from the name, e.g., "15 augusti 16.55"
+                            date_match = re.search(r"(\S+) (\S+) (\d+\.\d+)", name_str)   
+                            if date_match:
+                                day = date_match.group(1)
+                                month = date_match.group(2)
+                                time = date_match.group(3)
+                                english_month = month_translation.get(month.lower())
+                                if not english_month:
+                                    logging.error("Failed month translation")
+                                    continue  # Skip this event if month is unrecognized
+
+                                date_time = f"{day} {english_month} {time}"
+                                # Parsing string into a datetime object
+                                event_time = datetime.strptime(date_time, "%d %B %H.%M")
+                                # If latest time is smaller than the event time | make the event time the latest
+                                if latest_time is None or event_time > latest_time:
+                                    latest_time = event_time
+                                    latest_event = event
+
                     if latest_event:
                         location = latest_event.get('location', {}).get('name', 'Unknown')
                         event_type = latest_event.get('type', 'Unknown')
                         event_time_str = re.search(r"([0-9]+.[0-9]+)", latest_event.get('name', 'Unknown')).group(1)
-    
-                        if not self.options['twoUi']:
-                            location_split = location.split()
-                            location = max(location_split, key=len)
-                            processed_words = [
-                                word[:15] if word == location else word[0]
-                                for word in location_split if "län" not in word
-                            ]
-                            result = ' '.join(processed_words)
-                            self.city = event_type
-                            self.event = f"{result} ({event_time_str})"
-                        else:
-                            self.city = event_type
-                            self.event = f"{location} ({event_time_str})"
-    
+
+                        self.city = f"{event_type}"
+                        self.event = f"{location} ({event_time_str})"
+
                         logging.info("[Polisen] Fetched news")
         except requests.RequestException as e:
             logging.error(f"[Polisen] Data fetch failed: {e}")
